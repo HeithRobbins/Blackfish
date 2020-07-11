a21983eddc1548743551d1260020330fc2794414
Heith <Heith.Robbins@gmail.com>
2020-07-11 12:54:17 -0600

    working on the navbar

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
+import NavigationComponent from "./navBar/navBar"
+
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationComponent/>
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
       </div>
